d45716a9558a838508d71d3a519a3a3460705970
regnarock <thomas.wilgenbus@gmail.com>
2014-06-14 23:36:06 +0900

    Reading FileSystem

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,43 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class Controller extends VBox {
+    @FXML
+    TreeView<PathItem> fileSystemTree;
+
+    @FXML
+    MenuItem openProject;
+
+    private File projectDir;
+    private Stage stage;
+
+    public Controller() {
+        //FileSystem.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+    }
+
+    public void initData(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void openProject(ActionEvent event) {
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("Projects chooser");
+        File defaultDirectory = new File("c:");
+        chooser.setInitialDirectory(defaultDirectory);
+
+        PathItem selectedDirectory = new PathItem(chooser.showDialog(stage).toPath());
+        TreeItem<PathItem> root = PathTreeItem.createNode(selectedDirectory);
+        System.out.println(root);
+        fileSystemTree.setRoot(root);
+    }
 }
